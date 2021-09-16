be3aad8bfa9caeabc625f47d8707d88f869223b6
Jeniferignatius <jeni.igni@gmail.com>
2021-09-16 14:42:17 +0300

    project3 added

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,12 +1,8 @@
-import React, { useState } from 'react';
-import { Projecth } from '../pages/projecthidden';
+import React from 'react';
+//import { Projecth } from '../pages/projecthidden';
 export const Projects = (props) => {
 
-    const [isOpen, setIsOpen] = useState(false);
- 
-    const togglePopup = () => {
-      setIsOpen(!isOpen);
-    }
+
     return (
       <div id='projects' className='text-center'>
         <div className='container'>
@@ -22,18 +18,6 @@ export const Projects = (props) => {
                 <div className='portfolio-item'>
                   <div className='hover-bg'>
                     {' '}
-                    {isOpen && <Projecth
-                      content={<>
-                      <b className="bold">Food online ordering system</b><br/><br/>
-                      <ul float-left>
-                        <li><b>Subject : </b><span>We can order the food from online. As an admin you can able to add the food items to the catagories.</span></li>
-                        <li><b>Program : </b><span>Bootstrap, PHP, SQL.</span></li>
-                        <li><b>Github code : </b></li>
-                      </ul> 
-                      <a href = {props.data ? props.data.azure : '/'}><button float-right>VIEW LIVE DEMO</button></a>
-                      </>}
-                       handleClose={togglePopup}
-                      />}
                     <a
                       href='img/project1.png'
                       title='Project Title'
@@ -41,6 +25,7 @@ export const Projects = (props) => {
                     >
                       <div className='hover-text'>
                         <h4>Food online ordering system.</h4>
+                        <h5>Bootstrap, PHP, SQL.</h5>
                       </div>
                       <img
                         src='img/project1.png'
@@ -49,12 +34,14 @@ export const Projects = (props) => {
                       />{' '}
                     </a>{' '}
                   </div>
-                  <h4>Food online ordering system.</h4>
-                  <p>Bootstrap, HTML, CSS, PHP.</p>
+              
                   <span>
-                      <button className='more' onClick={togglePopup} >
-                      know more
-                      </button></span>
+                      <a href = {props.data ? props.data.azure : '/'}>
+                      <button className='more' >
+                        Demo
+                      </button>
+                      </a>
+                      </span>
                 </div>
               </div>
              
@@ -62,18 +49,7 @@ export const Projects = (props) => {
                 <div className='portfolio-item'>
                   <div className='hover-bg'>
                     {' '}
-                    {isOpen && <Projecth
-                      content={<>
-                      <b className="bold">Email Template</b><br/><br/>
-                      <ul float-left>
-                        <li><b>Subject : </b><span>This is a simple email template.</span></li>
-                        <li><b>Program : </b><span>HTML, CSS.</span></li>
-                        <li><b>Github code : </b></li>
-                      </ul> 
-                      <a href = "http://jeniferi.azurewebsites.net"><button float-right>VIEW LIVE DEMO</button></a>
-                      </>}
-                       handleClose={togglePopup}
-                      />}
+                   
                     <a
                       href='img/project2.png'
                       title='Project Title'
@@ -81,6 +57,7 @@ export const Projects = (props) => {
                     >
                       <div className='hover-text'>
                         <h4>Email Template.</h4>
+                        <h5>HTML, CSS.</h5>
                       </div>
                       <img
                         src='img/project2.png'
@@ -90,16 +67,47 @@ export const Projects = (props) => {
                       />{' '}
                     </a>{' '}
                   </div>
-                  <h4>Email Template.</h4>
-                  <p>HTML, CSS.</p>
                   <span>
-                      <button className='more' onClick={togglePopup} >
-                      know more
-                      </button></span>
+                  <a href = {props.data ? props.data.etemplate : '/'}>
+                      <button className='more' >
+                        Demo
+                      </button>
+                  </a>
+                  </span>
                 </div>
               </div>
 
-             
+              <div className='col-sm-6 col-md-4 col-lg-4'>
+                <div className='portfolio-item'>
+                  <div className='hover-bg'>
+                    {' '}
+                   
+                    <a
+                      href='img/project3.png'
+                      title='Project Title'
+                      data-lightbox-gallery='gallery1'
+                    >
+                      <div className='hover-text'>
+                        <h4>Todo App.</h4>
+                        <h5>ReactJs.</h5>
+                      </div>
+                      <img
+                        src='img/project3.png'
+                        height = '20px'
+                        className='img-responsive'
+                        alt='Project Title'
+                      />{' '}
+                    </a>{' '}
+                  </div>
+                  <span>
+                  <a href = {props.data ? props.data.todoapp : '/'}>
+                      <button className='more' >
+                        Demo
+                      </button>
+                  </a>
+                  </span>
+                </div>
+              </div>
              
               
             </div>
